6253dd35813831ad6874c5cd0105050d9b44154b
sravya-99 <sravya99gg@gmail.com>
2026-05-15 17:59:09 +0530

    Updated student management Frontend

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function showMessage() {
-    alert("Welcome to AWS Cloud Project");
-}
+document.getElementById("studentForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    alert("Student Registered Successfully");
+
+});
